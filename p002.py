d79a88ab3cc2d94f2a3c291f1670dede038400ba
Ben Wilson <btw@pdx.edu>
2017-08-11 18:38:11 -0700

    Removed conditional statements

--- p002.py
+++ p002.py
@@ -6,20 +6,21 @@
 # exceed four million, find the sum of the even-valued terms.
 
 import time
+
+# Starting with 1,1,2,3,5,8,13,21,34... we can see that every third value
+# in the sequence is even and we can skip the conditional statements
+def evenFibonaciSum(top_limit):
+    total = 0
+    a, b = 0, 1
+    while a < top_limit:
+        total += a
+        a, b = b, a+b
+        a, b = b, a+b
+        a, b = b, a+b
+    return total
+
 start = time.time()
-
-a = 0
-b = 1
-fib = a + b
-
-total = 0
-while fib < 4e6:
-	if not fib % 2:
-		total += fib
-	a = b
-	b = fib
-	fib = a + b
-end = time.time()
+total = evenFibonaciSum(4e6)
+stop = time.time()
 print(total)
-print('Processing Time: {} seconds'.format(end-start))
-
+print('Time: {} seconds'.format(stop - start))
