e95151e149ebac2c6af1a6335444bc80f35c5d2d
Ben Wilson <btw@pdx.edu>
2017-08-11 18:43:50 -0700

    Added notes

--- p005.py
+++ p005.py
@@ -4,9 +4,12 @@
 # this is evenly divisible by all of the numbers from 1 to 20?
 
 import time
-start = time.time()
 
+# Simply list the prime factors for integers from 1 - 20
+# There will be multiple 2s and 3s to accomodate 20, 16, 12, 8, 4, 18, and 9
+
+start = time.time()
 a = 19*17*13*11*7*5*3*2*2*2*2*3
-end = time.time()
+stop = time.time()
 print(a)
-print('Processing Time: {} seconds'.format(end-start))
+print('Time: {} seconds'.format(stop - start))
