c08eca698a3ea905a7fee08393bbf60efc5d521f
Ben Wilson <btw@pdx.edu>
2017-08-21 13:11:16 -0700

    Finished problem

--- p021.py
+++ p021.py
@@ -6,11 +6,48 @@
 # Evaluate the sum of all the amicable numbers under 10,000
 
 import time
-start = time.clock()
+import math
 
-num = 0
-tot = 0
-while num < 1e4:
-    num = 1e4
-end = time.clock()
-print('Processing Time: {} seconds'.format(end-start))
+
+def genPropDiv(bound):
+    # Generate dictionary with int keys 3x the bound
+    d = {}
+    for i in range(3*bound):
+        d[i] = 0
+
+    # Compute divisor sums from 2 up to the bound
+    for i in range(2, bound+1, 1):
+        # pdb.set_trace()
+        divsum = 1
+        fact = 2
+        f_max = math.sqrt(i)
+        while fact <= f_max:
+            if not i % fact:
+                f_low = fact
+                f_high = i // fact
+                if f_low == f_high:  # repeated factors
+                    f_high = 0
+                divsum += f_low + f_high
+            fact += 1
+        d[i] = divsum   # Update dictionary
+    return d
+
+
+def amicableNumSearch(d):
+    amic_sum = 0
+    perf_sum = 0
+    for i in d:
+        if d[d[i]] == i:    # Find amicable or perfect numbers
+            amic_sum += i
+            if d[i] == i:   # Find perfect numbers
+                perf_sum += i
+    return amic_sum - perf_sum  # Remove perfect numbers from sum
+
+
+start = time.time()
+bound = 10000
+a = genPropDiv(bound)
+amic_sum = amicableNumSearch(a)
+stop = time.time()
+print(amic_sum)
+print('Time: {} seconds'.format(stop-start))
