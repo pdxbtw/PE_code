83848120734cef9c58d3c17a555c186d72936c2d
Ben Wilson <btw@pdx.edu>
2017-08-21 13:14:16 -0700

    Updated formatting

--- p032.py
+++ p032.py
@@ -1,24 +1,17 @@
-#!/home/fure_j/Software/pypy3-2.4-linux_x86_64-portable/bin/pypy
-# !/home/wilson_b/anaconda3/bin/python
-
 # Project Euler - Problem 32
 # -----------------------------------------------------------------------------
 # We shall say than an n-digit number is pandigital if it makes use of all the
 # digits 1 to n exactly once; for example, the 5-digit number, 15234, is 1
 # through 5 pandigital.
-# The product 7254 is unusual, as the identity, 39x186=7254, containing 
+# The product 7254 is unusual, as the identity, 39x186=7254, containing
 # multiplicand, multiplier, and product is 1 through 9 pandigital.
 # Find the sum of all products whose multiplicand/multiplier/product identity
 # can be written as a 1 through 9 pandigital.
-# HINT: Some products can be obtained in more than one way so be sure to only 
+# HINT: Some products can be obtained in more than one way so be sure to only
 # include it on ce in your sum.
 
-from decimal import *
 import time
-import pdb
-getcontext().prec = 3
-#pdb.set_trace()
-start = time.clock()
+start = time.time()
 
 a = 1
 b = 0
@@ -26,24 +19,22 @@ dig_num = 0
 digits = ('1', '2', '3', '4', '5', '6', '7', '8', '9')
 num_l = []
 while a < 100:
-        a += 1
-        b = 10000//a
-        while a < b:
-                p = a * b
-                s = str(a) + str(b) + str(p)
-                len_s = len(s)
-                if len_s == 9:
-                        pan_dig = True
-                        for i in digits:
-                                if i not in s:
-                                        pan_dig = False
-                                        break
-                        if pan_dig:
-                                num_l += [p]
-                b -= 1
+    a += 1
+    b = 10000//a
+    while a < b:
+        p = a * b
+        s = str(a) + str(b) + str(p)
+        len_s = len(s)
+        if len_s == 9:
+            pan_dig = True
+            for i in digits:
+                if i not in s:
+                    pan_dig = False
+                    break
+            if pan_dig:
+                num_l += [p]
+        b -= 1
 num_l = set(num_l)
-end = time.clock()
+stop = time.time()
 print(sum(num_l))
-print("Processing time:", Decimal(end) - Decimal(start), "seconds")
-
-
+print("Time: {} seconds".format(stop-start))
--- p033.py
+++ p033.py
@@ -1,60 +1,52 @@
-#!/home/wilson_b/anaconda3/bin/python
-# !/home/fure_j/Software/pypy3-2.4-linux_x86_64-portable/bin/pypy^
-
 # Project Euler - Problem 33
 # -----------------------------------------------------------------------------
 # The fraction 49/98 is a curious fraction, as an inexperienced mathematician
 # in attempting to simplify it may incorrectly believe that 49/98 = 4/8, which
 # is correct, is obtained by cancelling the 9s.
 # We shall consider fractions like, 30/50=3/5, to be trivial examples.
-# There are exactly four non-trivial examples of this type of fraction, less 
-# than one in value, and containing two digits in the numerator and 
+# There are exactly four non-trivial examples of this type of fraction, less
+# than one in value, and containing two digits in the numerator and
 # denominator.
 # If the product of these four fractions is given in its lowest commmon term,
 # find the value of the denominator.
 
-from decimal import *
 import time
-import pdb
-getcontext().prec = 3
-#pdb.set_trace()
-start = time.clock()
+start = time.time()
 
 num_l = []
 den_l = []
 nums = ("1", "2", "3", "4", "5", "6", "7", "8", "9")
 for num in range(11, 99):
-        for den in range(11,99):
-                for i in nums:
-                        num_s = str(num)
-                        den_s = str(den)
-                        if (i in num_s) and (i in den_s):
-                                j = den_s.index(i)
-                                if j:
-                                        den_n = int(den_s[0])
-                                else:
-                                        den_n = int(den_s[1])
+    for den in range(11, 99):
+        for i in nums:
+            num_s = str(num)
+            den_s = str(den)
+            if (i in num_s) and (i in den_s):
+                j = den_s.index(i)
+                if j:
+                    den_n = int(den_s[0])
+                else:
+                    den_n = int(den_s[1])
 
-                                k = num_s.index(i)
-                                if k:
-                                        num_n = int(num_s[0])
-                                else:
-                                        num_n = int(num_s[1])
-                                if den_n != 0:
-                                        val_1 = num/den
-                                        val_2 = num_n/den_n
-                                        if (val_1 == val_2) and (val_1 < 1):
-                                                print("Value 1 =", str(num), "/", str(den), "\tValue 2 = ",
-                                                        str(num_n), "/", str(den_n))
-                                                time.sleep(0.5)
-                                                den_l += [den_n]
-                                                num_l += [num_n]
+                k = num_s.index(i)
+                if k:
+                    num_n = int(num_s[0])
+                else:
+                    num_n = int(num_s[1])
+                if den_n != 0:
+                    val_1 = num/den
+                    val_2 = num_n/den_n
+                    if (val_1 == val_2) and (val_1 < 1):
+                        print("Value 1 =", str(num), "/", str(den),
+                              "\tValue 2 = ", str(num_n), "/", str(den_n))
+                        time.sleep(0.5)
+                        den_l += [den_n]
+                        num_l += [num_n]
 prod_n = 1
 prod_d = 1
 for i in range(len(den_l)):
-        prod_n *= num_l[i]
-        prod_d *= den_l[i]
-end = time.clock()
+    prod_n *= num_l[i]
+    prod_d *= den_l[i]
+stop = time.time()
 print(str(prod_n), "/", str(prod_d))
-print("Processing time:", Decimal(end) - Decimal(start), "seconds")
-
+print("Time: {} seconds".format(stop-start))
